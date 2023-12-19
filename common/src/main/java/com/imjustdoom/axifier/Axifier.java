c17432506ea03581324aee8584bc6440475285d3
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-12-19 15:10:54 +1000

    Initial Version

--- common/src/main/java/com/imjustdoom/axifier/Axifier.java
+++ common/src/main/java/com/imjustdoom/axifier/Axifier.java
@@ -1,8 +1,51 @@
 package com.imjustdoom.axifier;
 
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.common.InteractionEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+
 public class Axifier {
     public static final String MOD_ID = "axifier";
 
     public static void init() {
+
+        InteractionEvent.INTERACT_ENTITY.register((player, entity, hand) -> {
+
+            if (player.getServer() == null) return EventResult.pass();
+            if (entity instanceof AgeableMob mob && !mob.isBaby() && player.getItemInHand(hand).is(ItemTags.AXES)) {
+                Level level = mob.level();
+                level.playSound(null, entity, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1f, 1f);
+
+                mob.setBaby(true);
+                mob.hurt(level.damageSources().generic(), 2f);
+
+                player.getItemInHand(hand).hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
+
+                if (!mob.isDeadOrDying()) {
+                    LootTable lootTable = player.getServer().getLootData().getLootTable(mob.getLootTable());
+                    LootParams.Builder builder = new LootParams.Builder((ServerLevel) level)
+                            .withParameter(LootContextParams.THIS_ENTITY, mob)
+                            .withParameter(LootContextParams.ORIGIN, mob.position())
+                            .withParameter(LootContextParams.DAMAGE_SOURCE, level.damageSources().generic())
+                            .withOptionalParameter(LootContextParams.KILLER_ENTITY, player)
+                            .withOptionalParameter(LootContextParams.DIRECT_KILLER_ENTITY, player);
+                    builder = builder.withParameter(LootContextParams.LAST_DAMAGE_PLAYER, player).withLuck(player.getLuck());
+                    LootParams lootParams = builder.create(LootContextParamSets.ENTITY);
+                    lootTable.getRandomItems(lootParams, mob.getLootTableSeed(), mob::spawnAtLocation);
+                }
+
+//                return EventResult.interruptDefault();
+            }
+            return EventResult.pass();
+        });
     }
 }
--- fabric/src/main/java/com/imjustdoom/axifier/fabric/AxifierFabric.java
+++ fabric/src/main/java/com/imjustdoom/axifier/fabric/AxifierFabric.java
@@ -1,52 +1,11 @@
 package com.imjustdoom.axifier.fabric;
 
 import com.imjustdoom.axifier.Axifier;
-import com.imjustdoom.axifier.block.BlockInit;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 
 public class AxifierFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Axifier.init();
-
-        FlammableBlockRegistry flammableBlockRegistry = FlammableBlockRegistry.getDefaultInstance();
-        flammableBlockRegistry.add(BlockInit.SUGAR_CANE_BLOCK.get(), 5, 20);
-        flammableBlockRegistry.add(BlockInit.SUGAR_CANE_SLAB.get(), 5, 20);
-        flammableBlockRegistry.add(BlockInit.SUGAR_CANE_STAIRS.get(), 5, 20);
-
-        flammableBlockRegistry.add(BlockInit.WHITE_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.ORANGE_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.MAGENTA_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIGHT_BLUE_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.YELLOW_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIME_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.PINK_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.GRAY_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIGHT_GRAY_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.CYAN_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.PURPLE_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BLUE_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BROWN_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.GREEN_WOOL_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.RED_SAND_STAIRS.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BLACK_WOOL_STAIRS.get(), 30, 60);
-
-        flammableBlockRegistry.add(BlockInit.WHITE_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.ORANGE_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.MAGENTA_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIGHT_BLUE_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.YELLOW_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIME_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.PINK_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.GRAY_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.LIGHT_GRAY_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.CYAN_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.PURPLE_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BLUE_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BROWN_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.GREEN_WOOL_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.RED_SAND_SLAB.get(), 30, 60);
-        flammableBlockRegistry.add(BlockInit.BLACK_WOOL_SLAB.get(), 30, 60);
     }
 }
