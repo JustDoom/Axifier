ca6ada93201d07996446f78f2f12d04f3cb78953
doom <61824552+JustDoom@users.noreply.github.com>
2024-06-10 17:11:18 +1000

    Get Forge working

---
+++ src/main/java/com/imjustdoom/axifier/Axifier.java
@@ -0,0 +1,36 @@
+package com.imjustdoom.axifier;
+
+import com.imjustdoom.axifier.config.Config;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+@Mod(Axifier.MODID)
+public class Axifier {
+
+    public static final String MODID = "axifier";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Axifier() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        try {
+            Config.init();
+        } catch (IOException exception) {
+            System.err.println("There was an error setting up or saving the config file for Axifier :(");
+            exception.printStackTrace();
+        }
+    }
+}
