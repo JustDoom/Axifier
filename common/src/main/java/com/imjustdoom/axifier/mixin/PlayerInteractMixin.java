5d8b0e11da98daa40abdf3e1182e2e394e691a2f
doom <61824552+JustDoom@users.noreply.github.com>
2024-07-20 13:23:11 +1000

    Move most duplicated code to its own method/slight cleanup

--- common/src/main/java/com/imjustdoom/axifier/mixin/PlayerInteractMixin.java
+++ common/src/main/java/com/imjustdoom/axifier/mixin/PlayerInteractMixin.java
@@ -10,8 +10,10 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -31,60 +33,39 @@ public abstract class PlayerInteractMixin {
 
         if (player.level().isClientSide() || Config.DISABLED_MOBS.contains(entity.getType())) return;
 
-        if (entity instanceof AgeableMob mob && !mob.isBaby() && player.getItemInHand(hand).is(ItemTags.AXES)) {
-            Level level = mob.level();
-            level.playSound(null, entity, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1f, 1f);
+        ItemStack handItem = player.getItemInHand(hand);
 
+        if (entity instanceof AgeableMob mob && !mob.isBaby() && handItem.is(ItemTags.AXES)) {
             mob.setBaby(true);
-
-            player.getItemInHand(hand).hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
-
-            if (Math.random() >= Config.SURVIVAL_CHANCE) {
-                mob.kill();
-                return;
-            }
-
-            mob.hurt(level.damageSources().generic(), Config.DAMAGE);
-
-            if (!mob.isDeadOrDying()) {
-                LootTable lootTable = player.getServer().getLootData().getLootTable(mob.getLootTable());
-                LootParams.Builder builder = new LootParams.Builder((ServerLevel) level)
-                        .withParameter(LootContextParams.THIS_ENTITY, mob)
-                        .withParameter(LootContextParams.ORIGIN, mob.position())
-                        .withParameter(LootContextParams.DAMAGE_SOURCE, level.damageSources().generic())
-                        .withOptionalParameter(LootContextParams.KILLER_ENTITY, player)
-                        .withOptionalParameter(LootContextParams.DIRECT_KILLER_ENTITY, player);
-                builder = builder.withParameter(LootContextParams.LAST_DAMAGE_PLAYER, player).withLuck(player.getLuck());
-                LootParams lootParams = builder.create(LootContextParamSets.ENTITY);
-                lootTable.getRandomItems(lootParams, mob.getLootTableSeed(), mob::spawnAtLocation);
-            }
-        } else if (player.getItemInHand(hand).is(ItemTags.AXES) && entity instanceof Zombie mob && !mob.isBaby()) {
-            Level level = mob.level();
-            level.playSound(null, entity, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1f, 1f);
-
+            everythingElse(handItem, player, hand, entity.level(), mob);
+        } else if (handItem.is(ItemTags.AXES) && entity instanceof Zombie mob && !mob.isBaby()) {
             mob.setBaby(true);
+            everythingElse(handItem, player, hand, entity.level(), mob);
+        }
+    }
 
-            player.getItemInHand(hand).hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
+    private void everythingElse(ItemStack handItem, Player player, InteractionHand hand, Level level, LivingEntity mob) {
+        level.playSound(null, mob, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1f, 1f);
+        handItem.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(hand));
 
-            if (Math.random() >= Config.SURVIVAL_CHANCE) {
-                mob.kill();
-                return;
-            }
+        if (Math.random() >= Config.SURVIVAL_CHANCE) {
+            mob.kill();
+            return;
+        }
 
-            mob.hurt(level.damageSources().generic(), Config.DAMAGE);
+        mob.hurt(level.damageSources().generic(), Config.DAMAGE);
 
-            if (!mob.isDeadOrDying()) {
-                LootTable lootTable = player.getServer().getLootData().getLootTable(mob.getLootTable());
-                LootParams.Builder builder = new LootParams.Builder((ServerLevel) level)
-                        .withParameter(LootContextParams.THIS_ENTITY, mob)
-                        .withParameter(LootContextParams.ORIGIN, mob.position())
-                        .withParameter(LootContextParams.DAMAGE_SOURCE, level.damageSources().generic())
-                        .withOptionalParameter(LootContextParams.KILLER_ENTITY, player)
-                        .withOptionalParameter(LootContextParams.DIRECT_KILLER_ENTITY, player);
-                builder = builder.withParameter(LootContextParams.LAST_DAMAGE_PLAYER, player).withLuck(player.getLuck());
-                LootParams lootParams = builder.create(LootContextParamSets.ENTITY);
-                lootTable.getRandomItems(lootParams, mob.getLootTableSeed(), mob::spawnAtLocation);
-            }
+        if (!mob.isDeadOrDying()) {
+            LootTable lootTable = player.getServer().getLootData().getLootTable(mob.getLootTable());
+            LootParams.Builder builder = new LootParams.Builder((ServerLevel) level)
+                    .withParameter(LootContextParams.THIS_ENTITY, mob)
+                    .withParameter(LootContextParams.ORIGIN, mob.position())
+                    .withParameter(LootContextParams.DAMAGE_SOURCE, level.damageSources().generic())
+                    .withOptionalParameter(LootContextParams.KILLER_ENTITY, player)
+                    .withOptionalParameter(LootContextParams.DIRECT_KILLER_ENTITY, player);
+            builder = builder.withParameter(LootContextParams.LAST_DAMAGE_PLAYER, player).withLuck(player.getLuck());
+            LootParams lootParams = builder.create(LootContextParamSets.ENTITY);
+            lootTable.getRandomItems(lootParams, mob.getLootTableSeed(), mob::spawnAtLocation);
         }
     }
 }
